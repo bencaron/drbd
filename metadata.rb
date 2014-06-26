23b295f2921e124c407f2259e134a4c538817a64
Benoit Caron <bcaron@lapresse.ca>
2014-06-26 11:08:27 -0400

    Bump version to 2.0 to show that we took ownership of the code

--- metadata.rb
+++ metadata.rb
@@ -4,7 +4,7 @@ maintainer_email  "cookbooks@opscode.com"
 license           "Apache 2.0"
 description       "Installs/Configures drbd."
 long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
-version           "0.8.3"
+version           "2.0.1"
 #depends           "lvm"
 depends           "yum"
 
